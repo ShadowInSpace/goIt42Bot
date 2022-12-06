28f2bbcbd61da158edd583bf21abf5625a3ec94e
MainKernel <86931698+MainKernel@users.noreply.github.com>
2022-12-06 15:20:59 +0200

    Update

--- src/main/java/com.jc8pg2.telegrambot/telegram/notification/NotifierStream.java
+++ src/main/java/com.jc8pg2.telegrambot/telegram/notification/NotifierStream.java
@@ -1,4 +1,20 @@
-package telegram.notification;
+package com.jc8pg2.telegrambot.telegram.notification;
+
+import com.jc8pg2.telegrambot.user.UserSettings;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class NotifierStream {
+
+    public void idler(UserSettings userSettings) {
+
+    }
+
+    public static void main(String[] args) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("HH:mm");
+        LocalDateTime now = LocalDateTime.now();
+        System.out.println(dtf.format(now));
+
+    }
 }
